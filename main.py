de64934d1b74e6553a4c089b1daeb3f2ea4acbd0
Abhishek <abhisheklinkd5@gmail.com>
2023-10-12 23:11:00 +0530

    stage first data ingestion completed

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from kidney_diseases_classification import logger
+from kidney_diseases_classification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to the custom log for this project.")
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
